5eff11f7643cd1209c1f55763ba7415a4dc2822f
Vadim_Zubenko <VZworkmail@yandex.ru>
2022-09-27 15:59:21 +0700

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень числа: ");
+int exponent = int.Parse(Console.ReadLine()!);
+
+int result = Power(number, exponent);
+Console.WriteLine(result);
+
+
+
+
+int Power(int num, int exp)
+{
+    int res = 1;
+    for (int i = 0; i < exp; i++)
+        res *= num;
+    return res;
+}
+
+
