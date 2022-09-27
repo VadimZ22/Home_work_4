9e4b448478eb817b926cd961841c07656f7d0c67
Vadim_Zubenko <VZworkmail@yandex.ru>
2022-09-27 17:33:58 +0700

    add task27

---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string strNumber = Console.ReadLine();
+int sum = SumNumbers(strNumber);
+Console.WriteLine(sum);
+
+
+
+int SumNumbers(string num)
+{
+    int number = Math.Abs(int.Parse(num));
+    num = number.ToString();
+    int len = num.Length;
+    int result = 0;
+    for (int i = 0; i < len; i++)
+        result = result + (int)Char.GetNumericValue(num[i]);
+    return result;
+}
