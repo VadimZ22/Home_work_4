d83e039a9b8b2f4ba348e8bef71d1e359c82f224
Vadim_Zubenko <VZworkmail@yandex.ru>
2022-09-27 17:47:10 +0700

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+int[] array = CreateArray();
+PrintArray(array);
+
+
+
+
+int[] CreateArray()
+{
+    int[] arr = new int[8];
+    Random rand = new Random();
+    for (int i = 0; i < 8; i++)
+    {
+        arr[i] = rand.Next(0, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(arr[i]);
+        if (i == count - 1)            
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
